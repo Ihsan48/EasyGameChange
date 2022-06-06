3cd70584c8834354ede1dad3aa4382deefdc193d
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-06-06 18:06:44 +0700

    Update

--- src/Ihsan48/SimpleGamemode/Loader.php
+++ src/Ihsan48/SimpleGamemode/Loader.php
@@ -8,39 +8,48 @@ use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Loader extends PluginBase {
+    
+    public function onEnable() : void {
+        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+    }
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
         switch ($cmd->getName()) {
             case "gmc":
                 if (!$sender->isCreative()) {
                     $sender->setGamemode(GameMode::CREATIVE());
-
+                    $sender->sendMessage($this->cfg->get("gamemode.creative"));
                 }
+                    
                 if (isset($args[0])) {
                     $target = $this->getServer()->getPlayerByPrefix($args[0]);
                     $target->setGamemode(GameMode::CREATIVE());
+                    $sender->sendMessage($this->cfg->get("gamemode.other.creative"));
                 }
                 break;
             case "gms":
                 if (!$sender->isSurvival()) {
                     $sender->setGamemode(GameMode::SURVIVAL());
-    
+                    $sender->sendMessage($this->cfg->get("gamemode.survival"));
                 }
                 if (isset($args[0])) {
                     $target = $this->getServer()->getPlayerByPrefix($args[0]);
                     $target->setGamemode(GameMode::SURVIVAL());
+                    $sender->sendMessage($this->cfg->get("gamemode.other.survival"));
                 }
                 break;
             case "gmspc":
                 if (!$sender->isSpectator()) {
                     $sender->setGamemode(GameMode::SPECTATOR());
-    
+                    $sender->sendMessage($this->cfg->get("gamemode.spectator"));
                 }
                 if (isset($args[0])) {
                     $target = $this->getServer()->getPlayerByPrefix($args[0]);
                     $target->setGamemode(GameMode::SPECTATOR());
+                    $sender->sendMessage($this->cfg->get("gamemode.other.spectator"));
                 }
                 break;
         }
